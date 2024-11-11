053d4642488d881057a867916d462f871530bb8c
TarcisioCl <tarcisiocluzfg@gmail.com>
2024-11-11 17:39:42 -0300

    JS para os links no nav no header, e menu de cada setor

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -1,3 +1,25 @@
+document.querySelectorAll(".toggle-link").forEach(link => {
+    link.addEventListener("click", (event) => {
+        event.preventDefault();
+        
+        // Extrair a classe do link clicado
+        const targetClass = `main-${link.id.replace('nav-', '')}`;
+        const targetDiv = document.querySelector(`.${targetClass}`);
+        
+        // Alternar a visibilidade da div correspondente
+        targetDiv.classList.toggle("main-hidden");
+        
+        document.querySelectorAll(".main-section").forEach(div => {
+            if (!div.classList.contains(targetClass)) {
+                div.classList.add("main-hidden");
+            }
+        });
+    });
+});
+
+
+
+
 
 
 
